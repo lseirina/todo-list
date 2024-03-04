41b97254f705ebe2f4a95dca43f5124e341a7f5c
root <lseirina87@gmail.com>
2024-03-04 21:56:14 +0300

    Added url for task

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('todos/user/', include('user.urls')),
     path('', include('core.urls')),
+    path('todos/task', include('task.urls'))
 ]
--- app/task/urls.py
+++ app/task/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from task.views import task_view
+from task import views
 
 app_name = 'task'
 
 urlpatterns = [
-    path('tasks/', task_view, name='task-list')
+    path('tasks/', views.task_view, name='tasks')
 ]
