41eee83c870057308ff65f2b5bd72ed8fea42840
root <lseirina87@gmail.com>
2024-03-11 22:18:22 +0300

    Added task_detail.html

--- app/task/urls.py
+++ app/task/urls.py
@@ -6,7 +6,7 @@ app_name = 'task'
 
 urlpatterns = [
     path('tasks/<int:task_id>', views.task_detail, name='task-detail'),
-    path('tasks/tasks', views.task_list, name='tasl-list'),
+    path('tasks', views.task_list, name='tasks-list'),
     path('tasks/task_update/<int:pk>', views.task_update, name='task-update'),
     path('tasks/delete/<int:pk>', views.task_delete, name='task-delete')
 ]
